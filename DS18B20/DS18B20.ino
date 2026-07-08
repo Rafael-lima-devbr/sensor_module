e822dd32b8c74ee443d0f49f8c1b9b82b9b08b01
Rafael Lima <rafaelima1250@gmail.com>
2026-07-08 18:33:05 -0300

    Fix variable name and improve setup logic

--- DS18B20/DS18B20.ino
+++ DS18B20/DS18B20.ino
@@ -1,21 +1,19 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-
 #define DS18B20_PIN 4
-
 
 OneWire onewire(DS18B20_PIN);
 DallasTemperature sensorTemperatura(&onewire);
 
-
 void setup() {
   Serial.begin(115200);
-  while(!sensorTemperature.begin()) {
+  sensorTemperatura.begin();
+  while(sensorTemperatura.getDeviceCount() == 0) {
     Serial.println("Aguardando DS18B20...");
     delay(1000);
   }
-  Serial.println("DS18B20 conectado!")
+  Serial.println("DS18B20 conectado!");
 }
 
 
@@ -23,9 +21,9 @@ void setup() {
 
 void loop () {
   sensorTemperatura.requestTemperatures();
-  float temperature = sensorTemperatura.getTempCByIndex(0);
+  float temperatura = sensorTemperatura.getTempCByIndex(0);
   Serial.print("Temperatura: ");
   Serial.print(temperatura);
-  Serial.print(" °C")
+  Serial.print(" °C");
 }
 
