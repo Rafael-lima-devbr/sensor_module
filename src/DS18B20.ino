60c067eaf4219693f2e3ce519d548ecdedf0a2b0
Rafael Lima <rafaelima1250@gmail.com>
2026-07-25 19:08:47 -0300

    Refactor temperature display and add filtering
    Refactored temperature display functions and added moving average calculation.

--- src/DS18B20.ino
+++ src/DS18B20.ino
@@ -6,8 +6,7 @@
 OneWire oneWire(DS18B20_PIN);
 DallasTemperature sensorTemperatura(&oneWire);
 
-
-float temperatura;
+float temperatura, temperatura_filtrada;
 
 void inicializar_temperatura() {
   sensorTemperatura.begin();
@@ -21,11 +20,19 @@ void inicializar_temperatura() {
 void medir_temperatura () {
   sensorTemperatura.requestTemperatures();
   temperatura = sensorTemperatura.getTempCByIndex(0);
+  temperatura_filtrada = mediaMovel(temperatura_media, temperatura);
 }
 
-void mostrar_temperatura() {
+void mostrar_temperatura_atual() {
   medir_temperatura();
   Serial.print("Temperatura: ");
   Serial.print(temperatura);
+  Serial.println(" °C");
+}
+
+void mostrar_temperatura_media() {
+  medir_temperatura();
+  Serial.print("Temperatura média: ");
+  Serial.print(temperatura_filtrada);
   Serial.println(" °C");
 }
