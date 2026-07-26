95263723853c68f74819137c540f90e6a71bc384
Rafael Lima <rafaelima1250@gmail.com>
2026-07-26 18:51:06 -0300

    Remove gas level categorization from display functions
    Removed gas level categorization logic from display functions.

--- src/MQ-2.ino
+++ src/MQ-2.ino
@@ -1,8 +1,5 @@
 #define GAS_PIN 33
 
-float gas, gas_filtrado;
-String nivel;
-
 void inicializar_gas () {
   pinMode(GAS_PIN, INPUT);
 }
@@ -16,26 +13,14 @@ void medir_gas () {
 
 void mostrar_gas_atual () {
   medir_gas();
-  if (gas < 30) nivel = "Muito baixa";
-  else if (gas < 40) nivel = "Baixa";
-  else if (gas < 60) nivel = "Moderada";
-  else if (gas < 80) nivel = "Alta";
-  else nivel = "Muito alta";
   Serial.print("Índice de detecção de gases: ");
   Serial.print(gas);
-  Serial.print("% | Nível: ");
-  Serial.println(nivel);
+  Serial.println(" ppm");
 }
 
 void mostrar_gas_media () {
   medir_gas();
-  if (gas_filtrado < 30) nivel = "Muito baixa";
-  else if (gas_filtrado < 40) nivel = "Baixa";
-  else if (gas_filtrado < 60) nivel = "Moderada";
-  else if (gas_filtrado < 80) nivel = "Alta";
-  else nivel = "Muito alta";
   Serial.print("Índice de detecção médio de gases: ");
   Serial.print(gas_filtrado);
-  Serial.print("% | Nível: ");
-  Serial.println(nivel);
+  Serial.println(" ppm");
 }
